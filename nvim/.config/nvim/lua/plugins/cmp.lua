7142d13214a0a8aa1b43225884336a9c20124dc4
Gustavo Fausto <gugapf08@gmail.com>
2026-05-08 19:25:00 -0300

    update: moving around snippets

--- nvim/.config/nvim/lua/plugins/cmp.lua
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -77,11 +77,11 @@ return {
 					["<C-Space>"] = cmp.mapping.complete(),
 					["<C-e>"] = cmp.mapping.abort(),
 
-					["<CR>"] = cmp.mapping.confirm({
+					["<C-l>"] = cmp.mapping.confirm({
 						select = true,
 					}),
 
-					["<Tab>"] = cmp.mapping(function(fallback)
+					["<C-j>"] = cmp.mapping(function(fallback)
 						if cmp.visible() then
 							cmp.select_next_item()
 						elseif luasnip.expand_or_jumpable() then
@@ -91,7 +91,7 @@ return {
 						end
 					end, { "i", "s" }),
 
-					["<S-Tab>"] = cmp.mapping(function(fallback)
+					["<C-k>"] = cmp.mapping(function(fallback)
 						if cmp.visible() then
 							cmp.select_prev_item()
 						elseif luasnip.jumpable(-1) then
